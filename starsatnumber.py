66ad7aca66ed0097487aeb38d4bb3b00b376b9c4
waynet-inc <valeriy.upw@gmail.com>
2020-12-21 16:41:29 +0500

    refactor: add docs & separate interface

--- starsatnumber.py
+++ starsatnumber.py
@@ -1,72 +1,32 @@
-# Скачать актуальные эфемериды спутников StarLink
-# и определить сколько спутников попадет в кадр снятый из КГО ГАИШ МГУ,
-# как функцию времени начала и конца экспозиции,
-# экваториальных координат центра изображения и
-# поле зрения телескопа.
-
-from skyfield.api import EarthSatellite, Topos, load
-from datetime import timedelta
+from skyfield.api import load
 from tkinter.ttk import *
 from tkinter import *
-import requests
+from satnum import satnum
 
 
-def update():
-    progress['value'] = 25
+def count(start_time, exposition, eq_cords, field_of_view):
+    res_bar['value'] = 0
     window.update_idletasks()
-
-    url = "https://celestrak.com/NORAD/elements/starlink.txt"
-    download = requests.get(url)
-
-    progress['value'] = 50
-    window.update_idletasks()
-
-    with open("starlink.txt", "wb") as f:
-        f.write(download.content)
-
-    progress['value'] = 100
-    window.update_idletasks()
-
-
-def satnum(start_time, exposition, eq_cords, field_of_view):
+    ts = load.timescale()
     date = start_time.get().split('-')
     date = list(map(int, date))
     start_time = ts.utc(*date)
     exposition = int(exposition.get())
     field_of_view = int(field_of_view.get())
+    eq_cords = eq_cords.get().split(' ')
+    eq_cords = list(map(float, eq_cords))
 
-    with open("starlink.txt") as file_input:
-        sat_counter = 0
-        res_bar['value'] = 0
+    res_bar['value'] = 50
+    window.update_idletasks()
+    answer = satnum(start_time, exposition, eq_cords, field_of_view)
+    result.config(text=str(answer))
+    res_bar['value'] = 100
+    window.update_idletasks()
 
-        for line in file_input:
-            name = line
-            line1 = next(file_input)
-            line2 = next(file_input)
-            satellite = EarthSatellite(line1, line2, name)
-
-            difference = satellite - eq_cords
-
-            for n in range(exposition):
-                time = ts.utc(start_time.utc_datetime() + timedelta(minutes=n))
-                topocentric = difference.at(time)
-                altaz = topocentric.altaz()
-                if altaz[0].degrees >= (90 - field_of_view/2):
-                    sat_counter += 1
-                    break
-
-            res_bar['value'] += 0.1105
-            window.update_idletasks()
-
-    result.config(text=sat_counter)
-
-
-ts = load.timescale()
-eq_cords = Topos("43.4410 N", "42.4003 E", elevation_m=2112)
 
 window = Tk()
 window.title("Satellites Number")
-window.geometry('520x640')
+window.geometry('720x640')
 window.config(bg="white")
 
 lbl = Label(window, text="Узнайте сколько спутников StarLink попадает в кадр КГО ГАИШ МГУ !", bg="white")
@@ -74,33 +34,31 @@ lbl.grid(row=0, columnspan=3, pady=20)
 
 time_label = Label(window, text="Время начала съемки: (ГГГГ-ММ-ДД-ЧЧ-ММ-СС)", bg="white")
 time_label.grid(column=0, row=1)
-start = Entry(window, width=19)
+start = Entry(window, width=42)
 start.insert(END, "2020-12-14-18-00-00")
 start.grid(column=1, row=1)
 
-exposition_label = Label(window, text="Выдержка: (в минутах)", bg="white")
+exposition_label = Label(window, text="Выдержка: (в секундах)", bg="white")
 exposition_label.grid(column=0, row=2)
-exp = Entry(window, width=19)
+exp = Entry(window, width=42)
 exp.insert(END, "10")
 exp.grid(column=1, row=2)
 
-fov_label = Label(window, text="Поле зрения телескопа: (в градусах)", bg="white")
+fov_label = Label(window, text="Поле зрения телескопа: (в минутах)", bg="white")
 fov_label.grid(column=0, row=3)
-fov = Entry(window, width=19)
-fov.insert(END, "45")
+fov = Entry(window, width=42)
+fov.insert(END, "40")
 fov.grid(column=1, row=3)
 
-upd_label = Label(window, text="Обновление эфемерид:", bg="white")
-upd_label.grid(column=0, row=4, pady=5)
-upd = Button(window, text="Update!", command=update)
-upd.grid(column=1, row=4, rowspan=3)
-
-progress = Progressbar(window, orient=HORIZONTAL, length=200, mode="determinate")
-progress.grid(column=0, row=5)
+center_label = Label(window, text="Координаты центра изображение: (Геоцентрические)", bg="white")
+center_label.grid(column=0, row=4)
+center = Entry(window, width=42)
+center.insert(END, "4741.54541529 -2257.71642333 4505.71410004")
+center.grid(column=1, row=4)
 
 cnt_label = Label(window, text="Посчитать спутники:", bg="white")
 cnt_label.grid(column=0, row=6, pady=5)
-cnt = Button(window, text="Count!", command=lambda: satnum(start, exp, eq_cords, fov))
+cnt = Button(window, text="Count!", command=lambda: count(start, exp, center, fov))
 cnt.grid(column=1, row=6, rowspan=3)
 
 res_bar = Progressbar(window, orient=HORIZONTAL, length=200, mode="determinate")
